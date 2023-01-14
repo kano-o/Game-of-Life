05249c5e034bda8f8c7071ac13a948177415927f
kano <steen4145@gmail.com>
2023-01-14 22:04:15 +0100

    project part 2 completed

--- life.py
+++ life.py
@@ -10,4 +10,36 @@ def random_state(width, height):
     random_life = [[random.randint(0,1) for x in range(width)] for y in range(height)]
     return random_life
 
-print(random_state(5, 5))
+width = 30
+height = 10
+
+#print(random_state(width, height))
+
+def render(board):
+    
+    out = ''
+    lines = ''
+
+    for x in range(width + 2):
+        
+        lines += '-'
+
+    out += lines
+    out += '\n'
+
+    for x in range(height):
+        out += '|'
+        for y in range(width):
+            if (board[x][y] == 1):
+                out += '\033[92m■\033[0m'
+            else:
+                out += '\033[91m■\033[0m'
+            
+        out += '|'
+        out += '\n'
+    
+    out += lines
+
+    print(out)
+
+render(random_state(width, height))
